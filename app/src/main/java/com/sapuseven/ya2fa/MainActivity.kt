f29dfd8c6f70cbf50ab7a4c7a8a7f337998330e1
SapuSeven <sapuseven@gmail.com>
2019-11-25 14:21:15 +0100

    Initial commit

---
+++ app/src/main/java/com/sapuseven/ya2fa/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.sapuseven.ya2fa
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
